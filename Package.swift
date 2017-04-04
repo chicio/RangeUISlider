482c3972ff17aeba1dbb262cf310106a9a0e8261
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2017-04-04 21:19:47 +0200

    Added package manager support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+//
+//  Package.swift
+//  RangeUISlider
+//
+//  Created by Fabrizio Duroni on 04/04/2017.
+//  2017 Fabrizio Duroni
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "RangeUISlider",
+    dependencies : [],
+    exclude: ["Tests"]
+)
+
+
