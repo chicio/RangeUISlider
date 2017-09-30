b639dbc27e264915f455713dcfff6a2d4c24c73a
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2017-09-30 16:04:37 +0200

    Updated code style: removed comments from bar and gradient (No public api) :rotating_light:

--- Source/Bar.swift
+++ Source/Bar.swift
@@ -9,62 +9,48 @@
 import Foundation
 
 class Bar: UIView {
-    /// Bar leading offset constraint.
     private(set) var leadingConstraint: NSLayoutConstraint = NSLayoutConstraint()
-    /// Bar trailing offset constraint.
     private(set) var trailingConstraint: NSLayoutConstraint = NSLayoutConstraint()
-    /// Bar height constraint.
     private(set) var heightConstraint: NSLayoutConstraint = NSLayoutConstraint()
-    /// Left knob reference. Unforce wrapped because it surely exist from in range slider (refernce in main view).
     private weak var leftKnob: Knob!
-    /// Right knob reference. Unforce wrapped because it surely exist from in range slider (refernce in main view).
     private weak var rightKnob: Knob!
-    /// Flag that indicates that the left knob has been hitted.
     private var leftKnobHitted: Bool = false
-    /// Flag that indicates that the right knob has been hitted.
     private var rightKnobHitted: Bool = false
-
-    /**
-     Method used to setup a bar. This methods returns all the constraints to be activated.
-     
-     - parameter leftKnob: the left knob of the bar.
-     - parameter rightKnob: the right knob of the bar.
-     - parameter leading: the leading constant value to be used when creating the leading constraint.
-     - parameter trailing: the trailing constant value to be used when creating the trailing constraint.
-     - parameter height: the height constant vallue to be used when creating the height constraint.
-     
-     - returns: an arrays of constraints to be activated.
-     */
+    
     func setup(leftKnob aLeftKnob: Knob,
                rightKnob aRightKnob: Knob,
                leading: CGFloat,
                trailing: CGFloat,
                height: CGFloat) -> [NSLayoutConstraint] {
-        
         leftKnob = aLeftKnob
         rightKnob = aRightKnob
         translatesAutoresizingMaskIntoConstraints = false
+        let barContraints = createConstraintsUsing(leading: leading, trailing: trailing, height: height)
+        return barContraints
+    }
+    
+    func createConstraintsUsing(leading: CGFloat, trailing: CGFloat, height: CGFloat) -> [NSLayoutConstraint]  {
         leadingConstraint = NSLayoutConstraint(item: self,
-                                                    attribute: .leading,
-                                                    relatedBy: .equal,
-                                                    toItem: superview,
-                                                    attribute: .leading,
-                                                    multiplier: 1.0,
-                                                    constant: leading)
+                                               attribute: .leading,
+                                               relatedBy: .equal,
+                                               toItem: superview,
+                                               attribute: .leading,
+                                               multiplier: 1.0,
+                                               constant: leading)
         trailingConstraint = NSLayoutConstraint(item: self,
-                                                     attribute: .trailing,
-                                                     relatedBy: .equal,
-                                                     toItem: superview,
-                                                     attribute: .trailing,
-                                                     multiplier: 1.0,
-                                                     constant: -1.0 * trailing)
+                                                attribute: .trailing,
+                                                relatedBy: .equal,
+                                                toItem: superview,
+                                                attribute: .trailing,
+                                                multiplier: 1.0,
+                                                constant: -1.0 * trailing)
         heightConstraint = NSLayoutConstraint(item: self,
-                                                   attribute: .height,
-                                                   relatedBy: .equal,
-                                                   toItem: nil,
-                                                   attribute: .notAnAttribute,
-                                                   multiplier: 1.0,
-                                                   constant: height)
+                                              attribute: .height,
+                                              relatedBy: .equal,
+                                              toItem: nil,
+                                              attribute: .notAnAttribute,
+                                              multiplier: 1.0,
+                                              constant: height)
         let barConstraints: [NSLayoutConstraint] = [
             leadingConstraint,
             trailingConstraint,
@@ -87,16 +73,6 @@ class Bar: UIView {
         return barConstraints
     }
     
-    /**
-     Overridden hitTest method to support out of bounds view.
-     In this case we have to manage the fact that the knobs must be
-     draggable also outside of the bar bounds.
-     
-     - parameter point: the hit point inside the bar.
-     - parameter event: the event that caused the hit.
-     
-     - returns the view that must manage the touch.
-     */
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         leftKnobHitted = hitTestFor(view: leftKnob, contains: point, with: event)
         rightKnobHitted = hitTestFor(view: rightKnob, contains: point, with: event)
@@ -136,31 +112,11 @@ class Bar: UIView {
         return rightKnob.xPositionConstraint.constant * -1 <= rightKnob.widthConstraint.constant
     }
     
-    /**
-     Hit test for a specific view if it contains the point of touch.
-     
-     - seealso: https://developer.apple.com/library/content/qa/qa2013/qa1812.html.
-     
-     - parameter view: the view to be tested for hit/not hit.
-     - parameter point: the hit point inside the bar.
-     - parameter event: the event that caused the hit.
-     
-     - returns: true if the view has been hitted.
-     */
     private func hitTestFor(view: UIView, contains point: CGPoint, with event: UIEvent?) -> Bool {
         let pointInViewCoordinateSpace = view.convert(point, from: self)
         return hasBeenHitted(view: view, in: pointInViewCoordinateSpace, with: event)
     }
     
-    /**
-     Check if a view or one of its descendant has been hitted.
-     
-     - parameter view: the view under test for hit.
-     - parameter point: the point toi be checked for test on view.
-     - parameter event: the event that fired the touch.
-     
-     - returns: true if the point is in the view, else false.
-     */
     private func hasBeenHitted(view: UIView, in point: CGPoint, with event: UIEvent?) -> Bool {
         if wasHitTestSuccessfulFor(view: view, in: point, with: event) {
             return true
--- Source/GradientView.swift
+++ Source/GradientView.swift
@@ -9,13 +9,8 @@
 import Foundation
 
 class GradientView: UIView {
-    /// An additional layer to manage gradient effects.
     lazy private(set) var gradient: CAGradientLayer = CAGradientLayer()
     
-    /**
-     Layout subviews. In this case we need to layout the added gradient layer to get the size of the container.
-     Disable also the CA default animation.
-     */
     override func layoutSubviews() {
         CATransaction.begin()
         CATransaction.setDisableActions(true)
@@ -24,23 +19,11 @@ class GradientView: UIView {
         CATransaction.setDisableActions(false)
     }
     
-    /**
-     Add a gradient to the view. This method execute the setup of the gradientLayer property, using data received.
-     
-     - parameter firstColor: the first color of the gradient.
-     - parameter secondColor: the second color of the gradient.
-     - parameter startPoint: the starting point of the gradient.
-     - parameter endPoint: the end point of the gradient.
-     - parameter cornerRadius: the corner radius inherited from the container. Default 0.0.
-     */
     func addGradient(firstColor: UIColor?,
-                                 secondColor: UIColor?,
-                                 startPoint: CGPoint?,
-                                 endPoint: CGPoint?,
-                                 cornerRadius: CGFloat = 0.0) {
-        guard firstColor != nil && secondColor != nil else {
-            return
-        }
+                     secondColor: UIColor?,
+                     startPoint: CGPoint?,
+                     endPoint: CGPoint?,
+                     cornerRadius: CGFloat = 0.0) {
         let color1 = firstColor ?? UIColor(red: 140.0/255.0, green: 140.0/255.0, blue: 140.0/255.0, alpha: 1.0)
         let color2 = secondColor ?? UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)
         let begin = startPoint ?? CGPoint(x: 0.0, y: 0.5)
