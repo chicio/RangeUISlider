74004b04390f6d0ad464f4835e81e925364744da
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2022-03-24 20:10:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RangeUISlider",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "RangeUISlider", targets: ["RangeUISlider"])
